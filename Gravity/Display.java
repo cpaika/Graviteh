7b7de17d0bec2a3cbc0ecaf0916c7d7d1efd1124
cpaika <cpaika@umass.edu>
2014-04-02 21:17:15 +1100

    Adding in display logic

--- Gravity/Display.java
+++ Gravity/Display.java
@@ -1,5 +1,26 @@
 package Gravity;
 
-public class Display {
-
+public class Display 
+{
+	int startX, startY;
+	int endX, endY;
+	
+	/**
+	 * Takes in the width and height of the screen
+	 */
+	private Display(int width, int height)
+	{
+		startX = startY = 0;
+		endX = width;
+		endY = height;
+	}
+	
+	public void draw(Body x,int absX, int absY)//Fix sprite
+	{
+		//TODO: Rework this math to make sure that it includes all of the image
+		if(((absX > startX) && (absX < endX)) || ((absY > startY) && (absY < endY)))//if the object about to be displayed is inside the screen
+		{
+			
+		}
+	}
 }
